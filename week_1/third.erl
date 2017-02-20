770957a46114555a9621035fa512070d7f3cb850
DanCouper <danielcouper81@googlemail.com>
2017-02-20 23:06:19 +0000

    Added xOr exercise

---
+++ week_1/third.erl
@@ -0,0 +1,41 @@
+-module(third).
+-export([xOr_1/2,xOr_2/2,xOr_3/2,xOr_4/2,xOr_5/2,xOr_6/2]).
+
+
+xOr_1(true,false) ->
+  true;
+xOr_1(false,true) ->
+  true;
+xOr_1(_,_) ->
+  false.
+
+
+xOr_2(X,X) ->
+  false;
+xOr_2(_,_) ->
+  true.
+
+
+xOr_3(X,Y) when X =/= Y ->
+  true;
+xOr_3(_,_) ->
+  false.
+
+
+xOr_4(X,Y) ->
+  if
+    X == Y ->
+      false;
+    X =/= Y ->
+      true
+  end.
+
+xOr_5(X,Y) ->
+  X xor Y.
+
+
+xOr_6(X,Y) ->
+  case X and Y of
+    true -> false;
+    false -> true
+  end.
